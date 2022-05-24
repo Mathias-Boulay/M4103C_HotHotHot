8a787605142d46d122aab6cd81d032d6a6b914c4
SebastienDethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-05-24 16:46:38 +0200

    raw utils implementation

--- assets/js/utils/utils.js
+++ assets/js/utils/utils.js
@@ -1,4 +1,4 @@
-export function qs(element){return document.querySelector(element);}
+// DOM
 
 export function qsa(element, parent = document){return [...parent.querySelectorAll(element)];}
 
@@ -16,4 +16,55 @@ export function addGlobalEventListener(
         },
         options
     )
+}
+
+export function createElement(type, options = {}) {
+    const element = document.createElement(type);
+    Object.entries(options).forEach(([key, value]) => {
+        if (key === "class") {
+            element.classList.add(value);
+            return;
+        }
+        if (key === "dataset") {
+            Object.entries(options).forEach(([dataKey, dataValue]) => {
+            element.dataSet[dataKey] = dataValue;
+            })
+            return;
+        }
+        if (key === "text") {
+            element.textContent = value;
+            return;
+        }
+        element.setAttribute(key, value);
+    })
+    return element;
+}
+
+// Other
+
+export function randomNumberBetween(min, max) {return Math.floor(Math.random() * (max - min + 1) + min);}
+
+export function sleep(duration) {return new Promise(resolve => setTimeout(resolve, duration));}
+
+export function firstElementsArray(array, n = 1) {
+    if(n ===1)return array[0];
+    return array.filter((_, index) => index < n );
+}
+
+export function lastElementsArray(array, n = 1) {
+    if(n ===1)return array[array.length - 1];
+    return array.filter((_, index) => array.length - index <= n );
+}
+
+export function sampleInArray(array) {
+    return array[randomNumberBetween(0, array.length - 1)];
+}
+
+export function findByKeyArray(array, key) {return array.map(element => element[key]);}
+
+export function groupByKeyArray(array, key){
+    return array.reduce((group, element) => {
+        const keyValue = element[key];
+        return { ...group, [keyValue]: [...(group[keyValue] ?? []), element] }        
+        }, {})
 }
