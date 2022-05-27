7547b36d8214887cc32f84245e9c190eca60e530
SebastienDethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-05-27 21:28:03 +0200

    Final comments on constructHTML()

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,35 +7,36 @@ export class Application {
     constructor() {
         this.tabPosition = "left";
 
-        /* Création de l'intégralité des éléments HTML de base */
+        /* Création des éléments HTML*/
+                                               // ____________________________________________________________________________________________________________________
+        this.constructHTML = () => {           // |Type dElément|________________________Attributs, Datasets & Inner Texts________________________|___Elément Parent__|
+                                               //                                                                                                 (def: document.body)|
+            this.header           =  createElement(  "section", {id:"header"                                                                      }                  );
 
-        this.constructHTML = () => {
-            this.header           =  createElement(   "section", {   id:"header"}                                                                             );
+            this.userName         =  createElement(   "span",   {id:"userName",                        text:"Mitchel"                             },           header);
 
-            this.userName         =  createElement(    "span",   {   id:"userName",                             text:"Michel"        },                header );
+            this.connectionState  =  createElement(   "span",   {id:"connectionState",                 text:"Déconnexion"                         },           header);
 
-            this.connectionState  =  createElement(    "span",   {   id:"connectionState",                      text:"Deconnexion"   },                header );
+            this.tileMenu         =  createElement(    "ul",    {class:"tabMenu",                                                  role:"tablist" }                  );
 
-            this.tileMenu         =  createElement(     "ul",    {   class:"tabMenu",                           role:"tablist"       }                        );
+            this.tileHome         =  createElement(    "li",    {"data-target":"tabHome",              class:"active",             role:"tab"     },    this.tileMenu);
+            const linkTileHome    =  createElement(    "a",     {href:"#",                             text:"Accueil"                             },    this.tileHome);
 
-            this.tileHome         =  createElement(     "li",    {   class:"active", "data-target":"tabHome",   role:"tab"           },         this.tileMenu );
-            const linkTileHome    =  createElement(     "a",     {   href:"#",                                  text:"Accueil"       },         this.tileHome );
+            this.tileHistory      =  createElement(    "li",    {"data-target":"tabHistory",                                       role:"tab"     },    this.tileMenu);
+            const linkTileHistory =  createElement(    "a",     {href:"#",                             text:"Historique"                          }, this.tileHistory);
 
-            this.tileHistory      =  createElement(     "li",    {   "data-target":"tabHistory",                role:"tab"           },         this.tileMenu );
-            const linkTileHistory =  createElement(     "a",     {   href:"#",                                  text:"Historique"    },      this.tileHistory );
+            this.tileAlerts       =  createElement(    "li",    {"data-target":"tabAlerts",                                        role:"tab"     },    this.tileMenu);
+            const linkTileAlerts  =  createElement(    "a",     {href:"#",                             text:"Alertes"                             },  this.tileAlerts);
 
-            this.tileAlerts       =  createElement(     "li",    {   "data-target":"tabAlerts",                 role:"tab"           },         this.tileMenu );
-            const linkTileAlerts  =  createElement(     "a",     {   href:"#",                                  text:"Alertes"       },       this.tileAlerts );
+            this.links            =       qsa     (".tabMenu li"                                                                                                     );
 
-            this.links            =       qsa     ( ".tabMenu li"                                                                                             );
+            this.tabs             =  createElement(   "div",    {class:"tabs"                                                                     }                  );
 
-            this.tabs             =  createElement(    "div",    {   class:"tabs"                                                    }                        );
+            this.tabHome          =  createElement(   "div",    {id:"tabHome",   class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},        this.tabs);
+            this.tabHistory       =  createElement(   "div",    {id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},        this.tabs);
+            this.tabAlerts        =  createElement(   "div",    {id:"tabAlerts", class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},        this.tabs);
 
-            this.tabHome          =  createElement(    "div",    {id:"tabHome",   class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},this.tabs);
-            this.tabHistory       =  createElement(    "div",    {id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},this.tabs);
-            this.tabAlerts        =  createElement(    "div",    {id:"tabAlerts", class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},this.tabs);
-
-            this.contents         =       qsa     ( ".tabContent"                                                                                             );
+            this.contents         =       qsa     (".tabContent"                                                                                                     );
         }
 
         /* Comportement des onglets (tabs) */
@@ -43,27 +44,27 @@ export class Application {
         this.setTabsBehavior = () => {
             let firstTime = false; // permet de ne pas déclencher l'animation au premier affichage de l'onglet Accueil (Home)
             
-            const L2RI = "leftToRightIn  .4s forwards ease-in-out";
-            const L2RO = "leftToRightOut .4s forwards ease-in-out";
-            const R2LI = "rightToLeftIn  .4s forwards ease-in-out";
-            const R2LO = "rightToLeftOut .4s forwards ease-in-out";
+            const OPT_TXT = ".4s forwards ease-in-out";
+            const L2RI    = "leftToRightIn"  + OPT_TXT;
+            const L2RO    = "leftToRightOut" + OPT_TXT;
+            const R2LI    = "rightToLeftIn"  + OPT_TXT;
+            const R2LO    = "rightToLeftOut" + OPT_TXT;
 
             const toggle = (targetId) => {
                 if (firstTime === false){ firstTime = true; return; }
-                this.contents.forEach( (element) => {
-                    
 
+                this.contents.forEach( (element) => {  
                     qs(`[data-target="${element.id}"]`).classList[element.id === targetId ? "add" : "remove"]("active");
                     element.classList[element.id === targetId ? "add" : "remove"]("active");
 
                     if(tabHome.classList.contains("active") && firstTime) {
-                        if (this.tabPosition !== "right") tabHistory.style.animation = L2RO;
+                        if (this.tabPosition !== "right")  tabHistory.style.animation = L2RO;
                         tabAlerts.style.animation       =  L2RO;
                         tabHome.style.animation         =  L2RI;
                         this.tabPosition                = "left";
                     }
                     if(tabHistory.classList.contains("active")) {
-                        if (this.tabPosition !== "right"){
+                        if (this.tabPosition !== "right") {
                             tabHome.style.animation     =  R2LO;                        
                             tabHistory.style.animation  =  R2LI;
                             return;
@@ -73,7 +74,7 @@ export class Application {
                         this.tabPosition                = "middle";
                     }
                     if(tabAlerts.classList.contains("active")) {
-                        if (this.tabPosition !== "middle") {tabHome.style.animation = R2LO;}                       
+                        if (this.tabPosition !== "middle") tabHome.style.animation = R2LO;                     
                         tabHistory.style.animation      =  R2LO;                  
                         tabAlerts.style.animation       =  R2LI;                        
                         this.tabPosition                = "right";
@@ -81,12 +82,8 @@ export class Application {
                 });
             }
             this.links.forEach( (link) => {
-                link.addEventListener( "click", () => {
-                    toggle(link.dataset.target);
-                });
-                if (link.className.includes("active")) {
-                    toggle(link.dataset.target);
-                }
+                link.addEventListener( "click", () => { toggle(link.dataset.target); });
+                if (link.className.includes("active"))  toggle(link.dataset.target);
             })
         }
         this.create = () => {
@@ -96,7 +93,7 @@ export class Application {
     }
 }
 
-/* Instanciation d'une application :*/
+/* Instanciation d'une application */
 
 /* -> * /
  
