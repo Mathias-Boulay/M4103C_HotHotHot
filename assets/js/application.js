724d3b6e343960b6c2e09d9de1f956da189867d5
SebastienDethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-05-28 14:02:59 +0200

    Code readability improvements

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,68 +5,69 @@ import { qs, qsa, createElement } from "./utils/utils.js";
 export class Application {
 
     constructor() {
+
         this.tabPosition = "tabHome";
 
         /* Création des éléments HTML */
-                                      //         ___________________________________________________________________________________________________________________
-        this.constructHTML = () => {  //         |Type Elément|                        Attributs, Datasets & Inner Texts                        |  Elément Parent  |
-                                      //         |____________|_________________________________________________________________________________(def:document.body)|
-            this.header           = createElement( "section" ,{id:"header"                                                                      }                 );
-            this.userName         = createElement(   "span"  ,{id:"userName"                     ,text:"Mitchel"                                },          header);
-            this.connectionState  = createElement(   "span"  ,{id:"connectionState"              ,text:"Déconnexion"                            },          header);
-            this.linksMenu        = createElement(    "ul"   ,{class:"tabMenu"                   ,                               role:"tablist" }                 );
-            this.linkHome         = createElement(    "li"   ,{"data-target":"tabHome"           ,class:"active",                role:"tab"     },  this.linksMenu);
-            const anchorHome      = createElement(    "a"    ,{href:"#"                          ,text:"Accueil"                                },   this.linkHome);
-            this.linkHistory      = createElement(    "li"   ,{"data-target":"tabHistory"        ,                               role:"tab"     },  this.linksMenu);
-            const anchorHistory   = createElement(    "a"    ,{href:"#"                          ,text:"Historique"                             },this.linkHistory);
-            this.linkAlerts       = createElement(    "li"   ,{"data-target":"tabAlerts"         ,                               role:"tab"     },  this.linksMenu);
-            const anchorAlerts    = createElement(    "a"    ,{href:"#"                          ,text:"Alertes"                                }, this.linkAlerts);
-            this.tabs             = createElement(   "div"   ,{class:"tabs"                                                                     }                 );
-            this.tabHome          = createElement(   "div"   ,{id:"tabHome",   class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},       this.tabs);
-            this.tabHistory       = createElement(   "div"   ,{id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},       this.tabs);
-            this.tabAlerts        = createElement(   "div"   ,{id:"tabAlerts", class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},       this.tabs);
+                                      //        _________________________________________________________________________________________________________________
+        this.constructHTML = () => {  //        |   Type   |                        Attributs, Datasets & Inner Texts                        |  Elément Parent  |
+                                      //        |d'Element_|_________________________________________________________________________________|_def:document.body|
+            this.header          = createElement("section",{id             :"header"                                                         }                 );
+            this.userName        = createElement(  "span" ,{id             :"userName"        ,text :"Mitchel"                               },          header);
+            this.connectionState = createElement(  "span" ,{id             :"connectionState" ,text :"Déconnexion"                           },          header);
+            this.linksMenu       = createElement(   "ul"  ,{class          :"tabMenu"         ,                               role:"tablist" }                 );
+            this.linkHome        = createElement(   "li"  ,{"data-target"  :"tabHome"         ,class:"active",                role:"tab"     },  this.linksMenu);
+            const anchorHome     = createElement(   "a"   ,{href           :"#"               ,text :"Accueil"                               },   this.linkHome);
+            this.linkHistory     = createElement(   "li"  ,{"data-target"  :"tabHistory"      ,                               role:"tab"     },  this.linksMenu);
+            const anchorHistory  = createElement(   "a"   ,{href           :"#"               ,text :"Historique"                            },this.linkHistory);
+            this.linkAlerts      = createElement(   "li"  ,{"data-target"  :"tabAlerts"       ,                               role:"tab"     },  this.linksMenu);
+            const anchorAlerts   = createElement(   "a"   ,{href           :"#"               ,text :"Alertes"                               }, this.linkAlerts);
+            this.tabs            = createElement(  "div"  ,{class          :"tabs"                                                           }                 );
+            this.tabHome         = createElement(  "div"  ,{id:"tabHome"   ,class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},       this.tabs);
+            this.tabHistory      = createElement(  "div"  ,{id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},       this.tabs);
+            this.tabAlerts       = createElement(  "div"  ,{id:"tabAlerts" ,class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},       this.tabs);
             
-            this.links            =      qsa     (".tabMenu li");
-            this.contents         =      qsa     (".tabContent");
+            this.links           =      qsa     (".tabMenu li");
+            this.contents        =      qsa     (".tabContent");
         }                                      
 
         /* Comportement des onglets (tabs) */
 
         this.setTabsBehavior = () => {
+
             let firstTime = false; // permet de ne pas déclencher l'animation au premier affichage de l'onglet Accueil (Home)
             
             const OPT_TXT = ".4s forwards ease-in-out";
-            const L2RI    = "leftToRightIn"  + OPT_TXT;
-            const L2RO    = "leftToRightOut" + OPT_TXT;
-            const R2LI    = "rightToLeftIn"  + OPT_TXT;
-            const R2LO    = "rightToLeftOut" + OPT_TXT;
+            const L2RI    = `leftToRightIn ${OPT_TXT}`;
+            const L2RO    = `leftToRightOut${OPT_TXT}`;
+            const R2LI    = `rightToLeftIn ${OPT_TXT}`;
+            const R2LO    = `rightToLeftOut${OPT_TXT}`;
 
             const toggle = (targetId) => {
-                if (firstTime === false){ firstTime = true; return; }
+                
+                if (firstUse === false) { firstUse = true; return; }
 
-                this.contents.forEach( (element) => {  
+                this.contents.forEach( (element) => {                      
                     qs(`[data-target="${element.id}"]`).classList[element.id === targetId ? "add" : "remove"]("active");
                     element.classList[element.id === targetId ? "add" : "remove"]("active");
-
-                    if(tabHome.classList.contains("active") && firstTime) {
-                        switch(this.tabPosition){
-                            case "tabHistory": tabHome.style.animation    = L2RI; tabHistory.style.animation = L2RO; break;
-                            case "tabAlerts" : tabHome.style.animation    = L2RI; tabAlerts.style.animation  = L2RO; break;
+                    if(tabHome.classList.contains("active") && firstUse) {
+                        switch(this.tabPosition) {
+                            case "tabHistory"    : tabHome.style.animation    = L2RI; tabHistory.style.animation = L2RO; break;
+                            case "tabAlerts"     : tabHome.style.animation    = L2RI; tabAlerts.style.animation  = L2RO; break;
                         }
                         this.tabPosition = "tabHome";
                     }
-
                     if(tabHistory.classList.contains("active")) {
-                        switch(this.tabPosition){
-                            case "tabHome"   : tabHistory.style.animation = R2LI; tabHome.style.animation    = R2LO; break;
-                            case "tabAlerts" : tabHistory.style.animation = L2RI; tabAlerts.style.animation  = L2RO; break;
+                        switch(this.tabPosition) {
+                            case "tabHome"       : tabHistory.style.animation = R2LI; tabHome.style.animation    = R2LO; break;
+                            case "tabAlerts"     : tabHistory.style.animation = L2RI; tabAlerts.style.animation  = L2RO; break;
                         }
                         this.tabPosition = "tabHistory";
                     }
                     if(tabAlerts.classList.contains("active")) {
-                        switch(this.tabPosition){
-                            case "tabHome"   : tabAlerts.style.animation  = R2LI; tabHome.style.animation    = R2LO; break;
-                            case "tabHistory": tabAlerts.style.animation  = R2LI; tabHistory.style.animation = R2LO; break;
+                        switch(this.tabPosition) {
+                            case "tabHome"       : tabAlerts.style.animation  = R2LI; tabHome.style.animation    = R2LO; break;
+                            case "tabHistory"    : tabAlerts.style.animation  = R2LI; tabHistory.style.animation = R2LO; break;
                         }
                         this.tabPosition = "tabAlerts";
                     }
@@ -86,7 +87,7 @@ export class Application {
 
 /* Instanciation d'une application */
 
-/* -> * /
+/* -> */
  
 const app = new Application();
 app.create(); 
