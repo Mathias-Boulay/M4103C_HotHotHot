dbc8e66ee8d6bfcafab58524474edeb0309ccde4
SebastienDethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-06-25 08:55:48 +0200

    application.js refactoring to object

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,182 +2,155 @@ import { qs, qsa, createElement, addGlobalEventListener } from "./utils/utils.js
 
 /* Classe de création de l'interface d'application */
 
+export class Application extends Object {
 
-export class Application {
+    #tmpEventClient = 0;
+    #prevPosX = 0;
 
     constructor() {
-        this.header;
-        this.userName;
-        this.connectionState;
-        this.linksMenu;  
-        this.linkHome;      
-        this.linkHistory; 
-        this.linkAlerts;   
-        this.switcher;    
-        this.tabsContainer;         
-        this.tabHome;        
-        this.tabHistory;     
-        this.tabAlerts;
-
+        super();
         this.previousTabPosition = "tabHome";
         this.screenWidth = window.innerWidth;
-        window.addEventListener("resize", ()=> {this.screenWidth = window.innerWidth;console.log(this.screenWidth)});
-
-        /* Création des éléments HTML */
-                                      //        ___________________________________________________________________________________________________________________
-        this.constructHTML = () => {  //        |   Type   |                        Attributs, Datasets & Inner Texts                        |    Elément Parent  |
-                                      //        |d'Element_|_________________________________________________________________________________|__def:document.body_|
-            this.header          = createElement("section",{id             :"header"                                                         }                   );
-            this.userName        = createElement(  "span" ,{id             :"userName"        ,text  :"Mitchel"                              },       this.header);
-            this.connectionState = createElement(  "span" ,{id             :"connectionState" ,text  :"Déconnexion"                          },       this.header);
-            this.linksMenu       = createElement(   "ul"  ,{class          :"linksMenu"       ,                               role :"tablist"}                   );
-            this.switcher        = createElement(  "span" ,{id             :"switcher"        ,draggable:true,                                              },    this.linksMenu);
-            this.linkHome        = createElement(   "li"  ,{"data-target"  :"tabHome"         ,class :"active"               ,role :"tab"    },    this.linksMenu);
-            const anchorHome     = createElement(   "a"   ,{                                   text  :"Accueil"              ,href :"#"      },     this.linkHome);
-            this.linkHistory     = createElement(   "li"  ,{"data-target"  :"tabHistory"                                     ,role :"tab"    },    this.linksMenu);
-            const anchorHistory  = createElement(   "a"   ,{                                   text  :"Historique"           ,href :"#"      },  this.linkHistory);
-            this.linkAlerts      = createElement(   "li"  ,{"data-target"  :"tabAlerts"                                      ,role :"tab"    },    this.linksMenu);
-            const anchorAlerts   = createElement(   "a"   ,{                                   text  :"Alertes"              ,href :"#"      },   this.linkAlerts);
-            this.tabsContainer   = createElement(  "div"  ,{class          :"tabsContainer"                                                  }                   );
-            this.tabHome         = createElement(  "div"  ,{id:"tabHome"   ,class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},this.tabsContainer);
-            this.tabHistory      = createElement(  "div"  ,{id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},this.tabsContainer);
-            this.tabAlerts       = createElement(  "div"  ,{id:"tabAlerts" ,class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},this.tabsContainer);
-            this.links           =      qsa     (".linksMenu li");
-            this.contents        =      qsa     (".tabContent");
-        }                                      
-
-        this.startup = () => {
-            const el =  document;
-            el.addEventListener('pointerdown', this.handleStart);
-            el.addEventListener('pointerup', this.handleEnd);
-            el.addEventListener('pointercancel', this.handleCancel);
-            el.addEventListener('pointermove', this.handleMove);
-            console.log('Initialisation.');
-        } 
-
-        let prevPosX=0;
-        let tmpEventClient =0;
-        const ongoingTouches = [];
-        this.handleStart =(evt) =>{
-            evt.preventDefault();
-            prevPosX = evt.clientX
-          }
-
-        this.handleMove = (evt) =>{
-            evt.preventDefault();           
-            tmpEventClient=evt.clientX
-        }
-
-          this.handleEnd = (evt) => {
-            evt.preventDefault();
-            let screenPortionToMove = this.screenWidth * 1 / 10;
-            let diffPos = prevPosX - evt.clientX;
-            if(this.previousTabPosition == "tabHome"){
-                if(diffPos > 0 && evt.pointerType == "mouse")  this.linkHistory.click()
-                console.log(diffPos)
-            }
-            else if(this.previousTabPosition == "tabHistory"){
-                if(diffPos > 0 && evt.pointerType == "mouse") this.linkAlerts.click()
-                if(diffPos  < 0 && evt.pointerType == "mouse")this.linkHome.click()
-            }
-            else if(this.previousTabPosition == "tabAlerts"){
-                if(diffPos > 0 && evt.pointerType == "mouse") this.linkAlerts.click()
-                if(diffPos  < 0 && evt.pointerType == "mouse")this.linkHistory.click()
-            }
-          }
-
-          this.handleCancel=(evt) =>{
-            let diffPos = tmpEventClient - prevPosX;
-
-            if(this.previousTabPosition == "tabHome" && diffPos < -8.5 && evt.pointerType == "touch") this.linkHistory.click();
-            
-            else if(this.previousTabPosition == "tabHistory"){
-                if(diffPos < -8.5 && evt.pointerType == "touch") this.linkAlerts.click();
-                else if(diffPos > 8.5 && evt.pointerType == "touch") this.linkHome.click();
-            }
-
-            else if(this.previousTabPosition == "tabAlerts" && diffPos > 8.5 && evt.pointerType == "touch")this.linkHistory.click();
-
-            evt.preventDefault();
-          }
-
-        /* Comportement des onglets */
-
-        this.setTabsBehavior = () => {
-            let  firstUse = false; // Permet de ne pas déclencher l'animation au premier affichage de l'onglet Accueil (Home)
-            
-            const OPT_TXT = ".7s forwards ease-in-out";
-            const OPT_TXT2 = ".7s forwards ease-in-out";
-
-            const L2RI    = `leftToRightIn  ${OPT_TXT}`;
-            const L2RO    = `leftToRightOut ${OPT_TXT}`;
-            const R2LI    = `rightToLeftIn  ${OPT_TXT}`;
-            const R2LO    = `rightToLeftOut ${OPT_TXT}`;
-
-            const HOHI    = `homeToHistory  ${OPT_TXT2}`;
-            const HIAL    = `historyToAlerts${OPT_TXT2}`;
-            const ALHI    = `alertsToHistory${OPT_TXT2}`;
-            const HIHO    = `historyToHome  ${OPT_TXT2}`;
-            const ALHO    = `alertsToHome   ${OPT_TXT2}`;
-            const HOAL    = `homeToAlerts   ${OPT_TXT2}`;
-
-
-            const toggle  = (targetId) => {
-                
-                if (firstUse === false) { firstUse = true;switcher.innerText="Accueil"; return; }
-
-                this.contents.forEach(  (element)  => {                      
-                    element.classList                                  [element.id === targetId ? "add" : "remove"]("active");
-                    qs(`[data-target="${element.id}"]`).classList      [element.id === targetId ? "add" : "remove"]("active");
-
-                    if(this.tabHome.classList.contains("active") && firstUse) {
-                        switch(this.previousTabPosition) {
-                            case "tabHistory" : tabHome.style.animation    = L2RI; tabHistory.style.animation          = L2RO; 
-                                                switcher.style.animation   = HIHO; switcher.innerText="Accueil"        ;break;
-                            case "tabAlerts"  : tabHome.style.animation    = L2RI; tabAlerts.style.animation           = L2RO;
-                                                switcher.style.animation   = ALHO; switcher.innerText="Accueil"        ;break;
-                        }
-                        this.previousTabPosition = "tabHome";
-                    }
-                    if(this.tabHistory.classList.contains("active")) {
-                        switch(this.previousTabPosition) {
-                            case "tabHome"    : tabHistory.style.animation = R2LI; tabHome.style.animation             = R2LO;
-                                                switcher.style.animation   = HOHI; switcher.innerText="  Historique  " ;break;
-                            case "tabAlerts"  : tabHistory.style.animation = L2RI; tabAlerts.style.animation           = L2RO;
-                                                switcher.style.animation   = ALHI; switcher.innerText="  Historique  " ;break;
-                        }
-                        this.previousTabPosition = "tabHistory";
-                    }
-                    if(this.tabAlerts.classList.contains("active")) {
-                        switch(this.previousTabPosition) {
-                            case "tabHome"    : tabAlerts.style.animation  = R2LI; tabHome.style.animation             = R2LO;
-                                                switcher.style.animation   = HOAL; switcher.innerText="Alertes"        ;break;
-                            case "tabHistory" : tabAlerts.style.animation  = R2LI; tabHistory.style.animation          = R2LO; 
-                                                switcher.style.animation   = HIAL; switcher.innerText="Alertes"        ;break;
-                        }
-                        this.previousTabPosition = "tabAlerts";
-                    }
-                })
-            }
-            this.links.forEach( (link) => {
-                link.addEventListener("click", () => { toggle(link.dataset.target); });
-                if (link.className.includes("active")) toggle(link.dataset.target);
-            })
-        };
-        
-        this.create = () => {
-            this.constructHTML();
-            this.setTabsBehavior();
-            this.startup()
-        };
+        window.addEventListener("resize", ()=> {this.screenWidth = window.innerWidth;});
     }
+
+    /* Création des éléments HTML */
+                                  //        ___________________________________________________________________________________________________________________
+    #constructHTML = () => {      //        |   Type   |                        Attributs, Datasets & Inner Texts                  |    Elément Parent  |
+                                  //        |d'Element_|_________________________________________________________________________________|__def:document.body_|
+        this.header          = createElement("section",{id             :"header"                                                         }                   );
+        this.userName        = createElement(  "span" ,{id             :"userName"        ,text  :"Mitchel"                              },       this.header);
+        this.connectionState = createElement(  "span" ,{id             :"connectionState" ,text  :"Déconnexion"                          },       this.header);
+        this.linksMenu       = createElement(   "ul"  ,{class          :"linksMenu"       ,                               role :"tablist"}                   );
+        this.switcher        = createElement(  "span" ,{id             :"switcher"        ,                                              },    this.linksMenu);
+        this.linkHome        = createElement(   "li"  ,{"data-target"  :"tabHome"         ,class :"active"               ,role :"tab"    },    this.linksMenu);
+        const anchorHome     = createElement(   "a"   ,{                                   text  :"Accueil"              ,href :"#"      },     this.linkHome);
+        this.linkHistory     = createElement(   "li"  ,{"data-target"  :"tabHistory"                                     ,role :"tab"    },    this.linksMenu);
+        const anchorHistory  = createElement(   "a"   ,{                                   text  :"Historique"           ,href :"#"      },  this.linkHistory);
+        this.linkAlerts      = createElement(   "li"  ,{"data-target"  :"tabAlerts"                                      ,role :"tab"    },    this.linksMenu);
+        const anchorAlerts   = createElement(   "a"   ,{                                   text  :"Alertes"              ,href :"#"      },   this.linkAlerts);
+        this.tabsContainer   = createElement(  "div"  ,{class          :"tabsContainer"                                                  }                   );
+        this.tabHome         = createElement(  "div"  ,{id:"tabHome"   ,class:"tabContent","aria-labelledby":   "tabHome",role:"tabpanel"},this.tabsContainer);
+        this.tabHistory      = createElement(  "div"  ,{id:"tabHistory",class:"tabContent","aria-labelledby":"tabHistory",role:"tabpanel"},this.tabsContainer);
+        this.tabAlerts       = createElement(  "div"  ,{id:"tabAlerts" ,class:"tabContent","aria-labelledby": "tabAlerts",role:"tabpanel"},this.tabsContainer);
+        this.links           =      qsa     (".linksMenu li");
+        this.contents        =      qsa     (".tabContent");
+    }
+
+    /* Comportement des onglets */
+    #setTabsBehavior = () => {
+        let  firstUse = false; // Permet de ne pas déclencher l'animation au premier affichage de l'onglet Accueil (Home)
+
+        const OPT_TXT = ".7s forwards ease-in-out";
+        const OPT_TXT2 = ".7s forwards ease-in-out";
+
+        const L2RI    = `leftToRightIn  ${OPT_TXT}`;
+        const L2RO    = `leftToRightOut ${OPT_TXT}`;
+        const R2LI    = `rightToLeftIn  ${OPT_TXT}`;
+        const R2LO    = `rightToLeftOut ${OPT_TXT}`;
+
+        const HOHI    = `homeToHistory  ${OPT_TXT2}`;
+        const HIAL    = `historyToAlerts${OPT_TXT2}`;
+        const ALHI    = `alertsToHistory${OPT_TXT2}`;
+        const HIHO    = `historyToHome  ${OPT_TXT2}`;
+        const ALHO    = `alertsToHome   ${OPT_TXT2}`;
+        const HOAL    = `homeToAlerts   ${OPT_TXT2}`;
+
+        const toggle  = (targetId) => {
+            if (firstUse === false) { firstUse = true;switcher.innerText="Accueil"; return; }
+
+            this.contents.forEach(  (element)  => {
+                element.classList                                  [element.id === targetId ? "add" : "remove"]("active");
+                qs(`[data-target="${element.id}"]`).classList      [element.id === targetId ? "add" : "remove"]("active");
+
+                if(this.tabHome.classList.contains("active") && firstUse) {
+                    switch(this.previousTabPosition) {
+                        case "tabHistory" : this.tabHome.style.animation    = L2RI; this.tabHistory.style.animation = L2RO;
+                            this.switcher.style.animation   = HIHO; this.switcher.innerText="Accueil"        ;break;
+                        case "tabAlerts"  :this. tabHome.style.animation    = L2RI;this. tabAlerts.style.animation = L2RO;
+                            this. switcher.style.animation   = ALHO; this.switcher.innerText="Accueil"        ;break;
+                    }
+                    this.previousTabPosition = "tabHome";
+                }
+                if(this.tabHistory.classList.contains("active")) {
+                    switch(this.previousTabPosition) {
+                        case "tabHome"    :this.tabHistory.style.animation = R2LI; this.tabHome.style.animation  = R2LO;
+                            this.switcher.style.animation   = HOHI; this.switcher.innerText="  Historique  " ;break;
+                        case "tabAlerts"  :this.tabHistory.style.animation = L2RI; this.tabAlerts.style.animation  = L2RO;
+                            this.switcher.style.animation   = ALHI; this.switcher.innerText="  Historique  " ;break;
+                    }
+                    this.previousTabPosition = "tabHistory";
+                }
+                if(this.tabAlerts.classList.contains("active")) {
+                    switch(this.previousTabPosition) {
+                        case "tabHome"    : this.tabAlerts.style.animation  = R2LI; this.tabHome.style.animation             = R2LO;
+                            this.switcher.style.animation   = HOAL; this.switcher.innerText="Alertes"        ;break;
+                        case "tabHistory" : this.tabAlerts.style.animation  = R2LI; this.tabHistory.style.animation          = R2LO;
+                            this.switcher.style.animation   = HIAL; this.switcher.innerText="Alertes"        ;break;
+                    }
+                    this.previousTabPosition = "tabAlerts";
+                }
+            })
+        }
+        this.links.forEach( (link) => {
+            link.addEventListener("click", () => { toggle(link.dataset.target); });
+            if (link.className.includes("active")) toggle(link.dataset.target);
+        })
+    };
+
+    #startup() {
+        const d =  document;
+        d.addEventListener('pointerdown', this.#handleStart.bind(this));
+        d.addEventListener('pointerup', this.#handleEnd.bind(this));
+        d.addEventListener('pointercancel', this.#handleCancel.bind(this));
+        d.addEventListener('pointermove', this.#handleMove.bind(this));
+        console.log('Initialisation.');
+    }
+
+    #handleStart(evt){
+        evt.preventDefault();
+        this.#prevPosX = evt.clientX
+    }
+
+
+    #handleMove(evt) {
+        evt.preventDefault();
+        this.#tmpEventClient=evt.clientX
+    }
+
+    #handleEnd(evt) {
+        evt.preventDefault();
+        let screenPortionToMove = this.screenWidth * 1 / 10;
+        let diffPos = this.#prevPosX - evt.clientX;
+        if(this.previousTabPosition == "tabHome"){
+            if(diffPos > 0 && evt.pointerType == "mouse")  this.linkHistory.click()
+        }
+        else if(this.previousTabPosition == "tabHistory"){
+            if(diffPos > 0 && evt.pointerType == "mouse") this.linkAlerts.click()
+            if(diffPos  < 0 && evt.pointerType == "mouse")this.linkHome.click()
+        }
+        else if(this.previousTabPosition == "tabAlerts"){
+            if(diffPos > 0 && evt.pointerType == "mouse") this.linkAlerts.click()
+            if(diffPos  < 0 && evt.pointerType == "mouse")this.linkHistory.click()
+        }
+    }
+
+    #handleCancel(evt) {
+        let diffPos = this.#tmpEventClient - this.#prevPosX;
+
+        if(this.previousTabPosition == "tabHome" && diffPos < -8.5 && evt.pointerType == "touch") this.linkHistory.click();
+        else if(this.previousTabPosition == "tabHistory"){
+            if(diffPos < -8.5 && evt.pointerType == "touch") this.linkAlerts.click();
+            else if(diffPos > 8.5 && evt.pointerType == "touch") this.linkHome.click();
+        }
+        else if(this.previousTabPosition == "tabAlerts" && diffPos > 8.5 && evt.pointerType == "touch")this.linkHistory.click();
+
+        evt.preventDefault();
+    }
+    
+    create() {
+        this.#constructHTML();
+        this.#setTabsBehavior();
+        this.#startup()
+    };
 }
-
-/* Instanciation d'une application */
-
-/* -> * /
- 
-const app = new Application();
-app.create();  
-
-/**/
