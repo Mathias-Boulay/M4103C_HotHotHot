1631b51af685e1d9cd49fc9a139e556518a25880
nicolas-jaubert <nicolas.jaubert@etu.univ-amu.fr>
2022-06-28 22:46:53 +0200

    Added comments on PushNotifications and ToastNotifications

--- assets/js/notification/PushNotification.js
+++ assets/js/notification/PushNotification.js
@@ -1,33 +1,39 @@
-import { receptor } from "./../Receptor";
+/**
+ * Class aimed at sending notifications to the user when he isn't actively on the window
+ */
+ import { receptor } from "./../Receptor";
 
-export class PushNotification extends Object{
-
-    #captorName;
-    #value;
-    static #notifImg = "./assets/images/android-chrome-192x192.png";
-    static #activeWindow = true;
-
-    constructor(){
-        super();
-        
-        receptor.addAlertListener(this);
-
-        window.addEventListener('focus',() => PushNotification.#activeWindow = true );
-        window.addEventListener('blur', () => PushNotification.#activeWindow = false);
-    }
-
-    update(sensorData){
-        this.#captorName = sensorData.Nom;
-        this.#value      = sensorData.Valeur;
-        if(!PushNotification.#activeWindow) 
-            Notification.requestPermission().then(result => { if(result === 'granted') this.#sendNotif();}); 
-    }
-
-    #sendNotif(){
-        const notifTitle = "Alerte pour le capteur " + this.#captorName;  
-        const notifBody = 'Température : ' + this.#value + '°C';   
-        const options = { body: notifBody, icon: PushNotification.#notifImg};
-        new Notification(notifTitle, options);
-    }
-
-}
+ export class PushNotification extends Object{
+ 
+     #captorName; /* Name of the captor which is at the origin of the alert */
+     #value; /* Value of the captor*/
+     static #notifImg = "./assets/images/android-chrome-192x192.png"; /* The location of the image show in all notifications by push */
+     static #activeWindow = true; /* static attribut storing if the user is on the window or not */
+ 
+     constructor(){
+         super();
+         
+         receptor.addAlertListener(this);
+ 
+         window.addEventListener('focus',() => PushNotification.#activeWindow = true );
+         window.addEventListener('blur', () => PushNotification.#activeWindow = false);
+     }
+ 
+     /** Proxy method called because we are a Receptor listener */
+     update(sensorData){
+         this.#captorName = sensorData.Nom;
+         this.#value      = sensorData.Valeur;
+         if(!PushNotification.#activeWindow) /* if the window isn't active, authorize sending notifications */
+             Notification.requestPermission().then(result => { if(result === 'granted') this.#sendNotif();}); 
+             /* check if the user has granted access before actually sending the notifications */
+     }
+ 
+     /** Formatting the data before sending the notification */
+     #sendNotif(){
+         const notifTitle = "Alerte pour le capteur " + this.#captorName;  
+         const notifBody = 'Température : ' + this.#value + '°C';   
+         const options = { body: notifBody, icon: PushNotification.#notifImg};
+         new Notification(notifTitle, options);
+     }
+ 
+ }
--- assets/js/notification/ToastNotification.js
+++ assets/js/notification/ToastNotification.js
@@ -1,42 +1,54 @@
-import { qs, createElement, removeElementChilds, sleep } from "./../utils/utils.js";
-import { obtainAlertContext, obtainDate } from "./../utils/alertUtils.js";
-import { receptor } from "./../Receptor";
-
-export class ToastNotification extends Object{
-
-    constructor(){
-        super();
-        receptor.addAlertListener(this);
-
-        const toastContainer   = createElement("div",{id:"toastContainer","aria-labelledby":"toastContainer",  role:"toastContainer"});
-        createElement("div",{id:"toastCaptorName","aria-labelledby":"toastCaptorName",role:"toastCaptorName"},toastContainer);
-        createElement("p",  {id:"toastContext",   "aria-labelledby":"toastContext",   role:"toastContext"   },toastContainer);
-        createElement("div",{id:"toastValue",     "aria-labelledby":"toastValue",     role:"toastValue"     },toastContainer);
-        createElement("div",{id:"toastDate",      "aria-labelledby":"toastDate",      role:"toastDate"      },toastContainer);
-        
-    }
-
-    update(sensorData){
-        if(qs("#tabAlerts").className.split(" ").includes("middle")) return;
-
-        this.showToast(sensorData.Nom, obtainAlertContext(sensorData.Nom, sensorData.Valeur), sensorData.Valeur, obtainDate(sensorData.Timestamp));
-    }
-
-    showToast(captorName, context, value, date){
-        const elementArray = ["CaptorName", "Context",  "Value", "Date"];
-        const contentArray = ["Capteur " + captorName, context, value + "°C", date];
-
-        let i = 0;
-        while (i < elementArray.length){
-            const element = qs("#toast" + elementArray[i]);
-            removeElementChilds(element);
-            element.append(contentArray[i]);
-            ++i;
-        }
-
-        const toastContainer = qs("#toastContainer");
-        toastContainer.addEventListener("click", () =>{qs("[data-target=tabAlerts]").click();})
-        toastContainer.className = "toastActive";
-        sleep(5000).then(resolve => {toastContainer.className = "toastInactive";});
-    }
-}
+/**
+ * Class aimed at showing a toast when an alert is received if the user isn't already on AlertsTabView
+ */
+ import { qs, createElement, removeElementChilds, sleep } from "./../utils/utils.js";
+ import { obtainAlertContext, obtainDate } from "./../utils/alertUtils.js";
+ import { receptor } from "./../Receptor";
+ 
+ export class ToastNotification extends Object{
+ 
+     constructor(){
+         super();
+         receptor.addAlertListener(this);
+ 
+         const toastContainer   = createElement("div",{id:"toastContainer","aria-labelledby":"toastContainer",  role:"toastContainer"});
+         createElement("div",{id:"toastCaptorName","aria-labelledby":"toastCaptorName",role:"toastCaptorName"},toastContainer);
+         createElement("p",  {id:"toastContext",   "aria-labelledby":"toastContext",   role:"toastContext"   },toastContainer);
+         createElement("div",{id:"toastValue",     "aria-labelledby":"toastValue",     role:"toastValue"     },toastContainer);
+         createElement("div",{id:"toastDate",      "aria-labelledby":"toastDate",      role:"toastDate"      },toastContainer);
+         
+     }
+ 
+     /** Proxy method called because we are a Receptor listener */
+     update(sensorData){
+         if(qs("#tabAlerts").className.split(" ").includes("middle")) return; /* To check whether or not the user is on AlertsTabView */
+ 
+         this.showToast(sensorData.Nom, obtainAlertContext(sensorData.Nom, sensorData.Valeur), sensorData.Valeur, obtainDate(sensorData.Timestamp));
+     }
+ 
+     /**
+      * Display the toast containing the alert data
+      * @param captorName Name of the captor linked to the alert
+      * @param context Context of the alert
+      * @param value Value of the alert
+      * @param date Date of the alert
+      */
+     showToast(captorName, context, value, date){
+         const elementArray = ["CaptorName", "Context",  "Value", "Date"];
+         const contentArray = ["Capteur " + captorName, context, value + "°C", date];
+ 
+         let i = 0;
+         while (i < elementArray.length){
+             const element = qs("#toast" + elementArray[i]);
+             removeElementChilds(element);
+             element.append(contentArray[i]);
+             ++i;
+         }
+ 
+         const toastContainer = qs("#toastContainer");
+         toastContainer.addEventListener("click", () =>{qs("[data-target=tabAlerts]").click();})
+         toastContainer.className = "toastActive";
+         sleep(5000).then(resolve => {toastContainer.className = "toastInactive";}); 
+         /* Let enough time to the toast so it can complete his animation*/
+     }
+ }
