7e8aca7498bbf2a6b773af1985e8b3417aa79d9d
Franck Daudet <franck@daudet.fr>
2022-06-30 17:28:00 +0200

    Graph V3

--- assets/js/graph/graph.js
+++ assets/js/graph/graph.js
@@ -1,6 +1,14 @@
 import { receptor } from "../Receptor";
 import {qs, createElement, getStartOfDay} from "../utils/utils";
 
+/*TODO
+ - Historique
+ - 
+ - Mettre les temp en valeur
+ - ToolTip 
+ - enlever Grid
+*/
+
 export class Graph {
 
     constructor(parent){
@@ -9,11 +17,11 @@ export class Graph {
 
         this.gradient = this.ctx.createLinearGradient(0, 0, 0, this.canvas.height);
         this.gradient.addColorStop(0, 'rgba(131,100,180,1)');
-        this.gradient.addColorStop(0.6, 'rgba(131,100,180,0.5)');
+        this.gradient.addColorStop(0.6, 'rgba(131,100,180,0)');
         this.gradient.addColorStop(1, 'rgba(131,100,180,0)');
         this.gradient2 = this.ctx.createLinearGradient(0, 0, 0, this.canvas.height);
         this.gradient2.addColorStop(0, 'rgba(253,29,29,1)');
-        this.gradient2.addColorStop(0.6, 'rgba(253,29,29,0.5)');
+        this.gradient2.addColorStop(0.6, 'rgba(253,29,29,0)');
         this.gradient2.addColorStop(1, 'rgba(253,29,29,0)');
 
         this.screenWidth = window.innerWidth;
@@ -21,6 +29,8 @@ export class Graph {
             this.gradientupdate();
             this.chart.update();
         });
+
+        this.tableCreate();
 
         this.data = {
         labels: [],
@@ -44,45 +54,50 @@ export class Graph {
             fill: "start",
             pointRadius: 0,
             tension: 0.5,
-            }
-            
+            } 
         ]
         };
-        this.config = {
-        type: 'line',
-        data: this.data,
-        options: {
-            spanGaps: true,
-            responsive:true,
-            maintainAspectRatio: false,
-            interaction: {
-                mode: 'index',
-                intersect: true,
-            },
-            stacked: false,
-            plugins: {
-                tooltip:{
-                    callbacks: {
-                        title: function(tooltipItem, data) {
-                            console.log(tooltipItem);
 
-                          return  "Capteur " ;
+        this.config = {
+            type: 'line',
+            data: this.data,
+            options: {
+                spanGaps: true,
+                responsive:true,
+                maintainAspectRatio: false,
+                stacked: false,
+                scales: {
+                    x :{
+                        grid:{
+                            display:false
                         },
-                        label: function(tooltipItem, data) {
-                          return tooltipItem.label;
-                        },
-                        afterLabel: function(tooltipItem, data) {
-                          return tooltipItem.formattedValue + " °C";
+                        ticks:{
+                            display: false
                         }
+                    },
+                    y :{
+                        grid:{
+                            drawBorder:false
+                        },
+                        ticks:{
+                            color: "#ffffff"
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: false,
+                        text: 'Chart.js Line Chart - External Tooltips'
                       },
+                      tooltip: {
+                        enabled: false,
+                        position: 'nearest',
+                      }
                 }
             }
-        }
         };
-        
     }
     
-    canvasUpdate
 
     formatDate(date) {
         var d = new Date(date),
@@ -92,7 +107,6 @@ export class Graph {
             minutes = d.getMinutes(),
             secondes = d.getSeconds(),
             ms = d.getMilliseconds();
-
         return [year,month,day,minutes,secondes].join('-');
     }
 
@@ -110,46 +124,46 @@ export class Graph {
                     dataset.data.push(null);
                 }
             });
-            
         });
         this.chart.update();
         }
+    
         addSingleData(date){
-                let tempDate = this.formatDate(date.Timestamp);
-                if(!(this.chart.data.labels).includes(tempDate)){
-                    (this.chart.data.labels).push(tempDate);
-                }
-                this.chart.data.datasets.forEach(dataset => {
-                    if(dataset.label === date.Nom){
-                        dataset.data.push(date.Valeur);
-                    }
-                    else if(this.chart.data.labels.label != dataset.data.length){
-                        dataset.data.push(null);
-                    }
-                });
-
-            this.chart.update();
+            console.log(date);
+            let tempDate = this.formatDate(date.Timestamp);
+            if(!(this.chart.data.labels).includes(tempDate)){
+                (this.chart.data.labels).push(tempDate);
             }
+            this.chart.data.datasets.forEach(dataset => {
+                if(dataset.label === date.Nom){
+                    dataset.data.push(date.Valeur);
+                }
+                else if(this.chart.data.labels.label != dataset.data.length){
+                    dataset.data.push(null);
+                }
+            });
+            this.chart.update();
+        }
 
     gradientupdate(){
         this.gradient = this.ctx.createLinearGradient(0, 0, 0, this.canvas.height);
         this.gradient.addColorStop(0, 'rgba(131,100,180,1)');
-        this.gradient.addColorStop(0.6, 'rgba(131,100,180,0.5)');
+        this.gradient.addColorStop(0.6, 'rgba(131,100,180,0)');
         this.gradient.addColorStop(1, 'rgba(131,100,180,0)');
 
         this.gradient2 = this.ctx.createLinearGradient(0, 0, 0, this.canvas.height);
 
         this.gradient2.addColorStop(0, 'rgba(253,29,29,1)');
-        this.gradient2.addColorStop(0.6, 'rgba(253,29,29,0.5)');
+        this.gradient2.addColorStop(0.6, 'rgba(253,29,29,0)');
         this.gradient2.addColorStop(1, 'rgba(253,29,29,0)');
         this.chart.data.datasets[1].backgroundColor = this.gradient2;
         this.chart.data.datasets[0].backgroundColor = this.gradient;
     }
+
     create(){
         this.chart = new Chart(this.ctx, this.config);
         receptor.addSensorDataListener(this)
         receptor.DAO.getSensorData({filters:["exterieur","interieur"],startTime: getStartOfDay(Date.now()).getTime()}).then((result) => {
-            console.log(result);
             this.addData(result);
         });
         this.gradientupdate();
@@ -159,4 +173,21 @@ export class Graph {
         this.addSingleData(t);
     }
 
-}
+    tableCreate(){
+        this.table = createElement("div",{role:"table", "aria-label":"Table des Températures en direct"}, this.canvas);
+        this.tableHeader = createElement("div",{role:"rowgroup"}, this.table);
+        createElement("span",{role:"columnheader",text:"Date"}, this.tableHeader);
+        createElement("span",{role:"columnheader",text:"Capteur Interne"}, this.tableHeader);
+        createElement("span",{role:"columnheader",text:"Capteur Externe"}, this.tableHeader);
+        this.tableData = createElement("div",{role:"rowgroup"}, this.table);
+        this.tableDatarow1 = createElement("div",{role:"row"}, this.tableData);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow1);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow1);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow1);
+        this.tableDatarow2 = createElement("div",{role:"row"}, this.tableData);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow2);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow2);
+        createElement("span",{role:"cell",text:"5 °c"}, this.tableDatarow2);
+    }
+
+}   
